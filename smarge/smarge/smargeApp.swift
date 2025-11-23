d5da7e4a4d7195d5ceaf424b3ec5e8c205c21715
Steve Lohr <schdief.law@gmail.com>
2025-11-23 19:20:09 +0100

    setup ios app

---
+++ smarge/smarge/smargeApp.swift
@@ -0,0 +1,17 @@
+//
+//  smargeApp.swift
+//  smarge
+//
+//  Created by Boobie on 23.11.25.
+//
+
+import SwiftUI
+
+@main
+struct smargeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
